096f01d8f0ffc306985b068f92c818c561321b06
Dmitry7777 <list-dmitrij@mail.ru>
2019-09-10 15:22:57 +0600

    Update FrequencyInput.h

--- FrequencyInput.h
+++ FrequencyInput.h
@@ -1,9 +1,10 @@
 #ifndef FREQUENCY_INPUT_H
 #define FREQUENCY_INPUT_H
 
-#define SYNC_FrequencyInput INPUT
+#define IO_MCU_FrequencyInput INPUT
 
 #define CFG_Time_PerSecond 10000
+int PROCESS_FrequencyInput; //
 long Temp = CFG_Time_PerSecond; //
 int Pulses = Temp; //
 
@@ -27,38 +28,38 @@ class FrequencyInput
 {
   public:
   FrequencyInput(int SynchInput){
-  float InputFrequ = SynchInput; //
-  pinMode(InputFrequ, SYNC_FrequencyInput); //
-  //FrequencyMode = digitalRead(InputFrequ); //
+  PROCESS_FrequencyInput = SynchInput; //
+  pinMode(SynchInput, IO_MCU_FrequencyInput); //
+  FrequencyMode = digitalRead(SynchInput); //
   };
   static void begin(uint16_t delay_ms){
     
   }; //
   //static uint8_t available(void){}; //
   static uint32_t read(void){
-//PulseLow = pulseIn(InputFrequ, LOW);
-//  PulseHigh = pulseIn(InputFrequ, HIGH);
+//    PulseLow = pulseIn(InputFrequ, LOW);
+//    PulseHigh = pulseIn(InputFrequ, HIGH);
     }; //
 
 void setLow(){
 //if(LF){}float LF
 
     Pulses = PulseLow + PulseHigh; //
-    ResultOfLowFrequency = Pulses / 1000;
+    ResultOfLowFrequency = Pulses / 2000;
     delay(Temp); //
 }
 
 void setMiddle(){
 //if(MF){}float MF
     Pulses = PulseLow + PulseHigh; //
-    ResultOfMiddleFrequency = Pulses / 1000000;
+    ResultOfMiddleFrequency = Pulses / 2000000;
     delay(Temp); //
 }
 
 void setHigh(){
   //if(HF){ }float HF
     Pulses = PulseLow + PulseHigh; //
-    ResultOfHighFrequency = Pulses / 1000000000;
+    ResultOfHighFrequency = Pulses / 2000000000;
     delay(Temp); //
 }
 
