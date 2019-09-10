10d17b9a8552a1c7011bea3ddb77f5c2b60e18c9
Dmitry7777 <list-dmitrij@mail.ru>
2019-09-10 17:59:08 +0600

    Update GeneralFrequencyInput.h

--- GeneralFrequencyInput.h
+++ GeneralFrequencyInput.h
@@ -1,1 +1,11 @@
+/*
+ * General Frequency Input
+ */
 
+#ifndef GeneralFrequencyInput_H
+#define GeneralFrequencyInput_H
+#include<Arduino.h>
+#include"FrequencyInput.h"
+#include"SAB6456.h"
+
+#endif
